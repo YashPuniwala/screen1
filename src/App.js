19e9e8cdb456fc9038fc35856eeadbf72009f050
Yash Puniwala <yashpuniwala@gmail.com>
2025-03-08 20:10:23 +0530

    Changed settingsLayout to MenuItemsPage

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import ContactLogin from "./components/contactLogin";
 import ConnectedAccounts from "./components/connectedAccounts";
 import Teams from "./components/teams";
 import Earning from "./components/earnings";
+import MenuItemsPage from "./components/menuItems";
+import Availability from "./components/availability";
+import Audience from "./components/audience";
+import Billing from "./components/billing";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         {/* Show SettingsLayout always with ContactLogin as default */}
-        <Route path="/" element={<SettingsLayout />}>
+        <Route path="/" element={<MenuItemsPage />}>
           <Route path="contact-login" element={<ContactLogin />} />
           <Route path="connected-accounts" element={<ConnectedAccounts />} />
           <Route path="teams" element={<Teams />} />
           <Route path="earnings" element={<Earning />} />
+          <Route path="availability" element={<Availability />} />
+          <Route path="audience" element={<Audience />} />
+          <Route path="billing" element={<Billing />} />
         </Route>
       </Routes>
     </Router>
